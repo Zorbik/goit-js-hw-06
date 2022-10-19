1c6910337014c4a412cf50c572e8fd8bca30f3e3
Andrii Zorba <andrej.zorba@gmail.com>
2022-10-19 11:21:28 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,10 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const button = document.querySelector(".change-color");
-button.addEventListener("click", handleColorOnBody);
+const refs = {
+  button: document.querySelector(".change-color"),
+  span: document.querySelector(".color"),
+};
+
+refs.button.addEventListener("click", handleColorOnBody);
 
 function handleColorOnBody() {
-  document.body.style.backgroundColor = getRandomHexColor();
-  document.querySelector(".color").textContent = getRandomHexColor();
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.span.textContent = color;
 }
